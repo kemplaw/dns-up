475ffe68608a16bbc74a87665c466587fa9190b9
kemplaw <kemplaw@hotmail.com>
2021-05-12 12:10:12 +0800

    update

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const { isIPv4, genUrlWithIPv4Reg } = require('./regexp')
     if (!matchedLines || matchedLines.length === 0) {
       console.log('no matched, will append')
 
-      const res = await writeFile(WINDOWS_HOSTS_PATH, `\n${newUrlMapStr}`, {
+      const res = await writeFile(WINDOWS_HOSTS_PATH, `\n${newUrlMapStr}\n`, {
         flags: 'a',
       })
 
